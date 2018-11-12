97f9d0a09d3901374a7b0e64a80b5ac05630f873
Small Rui <603049583@qq.com>
2018-11-12 23:24:55 +0800

    步进器-分栏控件

--- Day1/Day1/ViewController.h
+++ Day1/Day1/ViewController.h
@@ -9,7 +9,17 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+{
+    //    定义步骤器
+    //    按照一定的数字来调整某个数据
+    UIStepper* _stepper;
+    //    分栏控制器定义
+    UISegmentedControl* _segControl;
+    
+    
+}
+@property (retain,nonatomic) UIStepper* stepper;
+@property(retain,nonatomic) UISegmentedControl* segControl;
 
 @end
 
--- Day1/Day1/ViewController.m
+++ Day1/Day1/ViewController.m
@@ -14,12 +14,52 @@
 
 
 @implementation ViewController
+@synthesize stepper = _stepper;
+@synthesize segControl = _segControl;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-   
+    _stepper = [[UIStepper alloc]init];
+    //    设置位置 宽高不能变
+    _stepper.frame = CGRectMake(100, 50, 80, 40);
+    //    set 步进器 最小值
+    _stepper.minimumValue = 0;
+    //    set 步进器 最大值
+    _stepper.maximumValue = 15;
+    //    set当前值
+    _stepper.value = 0;
+    //    set 步进器每次向前或向后步进的步伐值
+    _stepper.stepValue = 5;
+    //    是否可以重复响应事件操作
+    _stepper.autorepeat = YES;
+    //    是否将步进器结果o通过事件函数响应出来
+    _stepper.continuous = YES;
+    //    添加事件函数
+    [_stepper addTarget:self action:@selector(stepChange) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:_stepper];
+    _segControl = [[UISegmentedControl alloc]init];
+    //    set 控件位置,宽度可变 高度不可变
+    _segControl.frame=CGRectMake(10, 100, 300, 40);
+    //    添加一个按钮元素
+    //    按钮选项文字
+    //    P2:按钮的索引位置
+    //    P3:是否有动画效果
+    [_segControl insertSegmentWithTitle:@"0元" atIndex:0 animated:YES];
+    [_segControl insertSegmentWithTitle:@"5元" atIndex:1 animated:YES];
+    [_segControl insertSegmentWithTitle:@"10元" atIndex:2 animated:YES];
+    [_segControl insertSegmentWithTitle:@"15元" atIndex:3 animated:YES];
+    _segControl.selectedSegmentIndex = 0;
+    [_segControl addTarget:self action:@selector(segChange) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:_segControl];
 }
 
-
+-(void)stepChange{
+    _segControl.selectedSegmentIndex = _stepper.value/5;
+    NSLog(@"step press! value=%f",_stepper.value);
+}
+-(void)segChange{
+    
+    NSLog(@"%ld",_segControl.selectedSegmentIndex);
+}
 @end
